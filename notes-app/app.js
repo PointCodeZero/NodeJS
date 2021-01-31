20f8431c589cc9af4ce542589c9a73253c045222
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 10:40:28 -0500

    print argv with addtional commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-console.log(getNotes());
-console.log(chalk.green.inverse.bold.italic('Success!'));
+const command = process.argv[2];
 
-console.log(process.argv[2])
+console.log(process.argv);
+
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}
