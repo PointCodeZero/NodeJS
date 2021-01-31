6e6daa37c2f7642c7651aed605f3b5339c4129c6
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 11:39:19 -0500

    handling JSON files

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
-const book = {
-  title: 'Ego is the enemy',
-  author: 'Ryan Holiday'
-};
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync('1-json.json', bookJSON);
-
+const dataBuffer = fs.readFileSync('1-json.json'); // Returns binary
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
 
 // Examples:
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// };
+
 // const bookJSON = JSON.stringify(book);
 // console.log(bookJSON);
 
 // const parsedData = JSON.parse(bookJSON);
 // console.log(parsedData.author);
+
+// Create JSON file
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
