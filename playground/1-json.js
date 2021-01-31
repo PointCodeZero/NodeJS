759ff1979e338b5bb4a5a1fd94768960c8c19d28
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 11:34:03 -0500

    create a JSON file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
 const book = {
   title: 'Ego is the enemy',
   author: 'Ryan Holiday'
 };
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+fs.writeFileSync('1-json.json', bookJSON);
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
+
+// Examples:
+// const bookJSON = JSON.stringify(book);
+// console.log(bookJSON);
+
+// const parsedData = JSON.parse(bookJSON);
+// console.log(parsedData.author);
