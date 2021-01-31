920e4b0f6c986fa6e040492f8db12b7300a1175d
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 11:51:29 -0500

    change existing JSON file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 
-const dataBuffer = fs.readFileSync('1-json.json'); // Returns binary
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title);
-
 // Examples:
 // const book = {
 //   title: 'Ego is the enemy',
@@ -20,3 +15,15 @@ console.log(data.title);
 // Create JSON file
 // const bookJSON = JSON.stringify(book);
 // fs.writeFileSync('1-json.json', bookJSON);
+
+// Read from JSON
+// const dataBuffer = fs.readFileSync('1-json.json'); // Returns binary
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+// Change existing JSON file
+const data = JSON.parse(fs.readFileSync('1-json.json').toString());
+data.name = 'Lucas';
+data.age = '32';
+fs.writeFileSync('1-json.json', JSON.stringify(data));
