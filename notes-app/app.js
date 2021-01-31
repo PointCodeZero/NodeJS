401fdc6f4af0c4fdac3a1604fafd788e09203727
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 10:56:37 -0500

    added yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,37 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+//Customize Yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add new note',
+  handler: () => console.log('Adding a new note!')
+});
 
-if (command === 'add') {
-  console.log('Adding note!');
-} else if (command === 'remove') {
-  console.log('Removing note!');
-}
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => console.log('Removing a note!')
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: () => console.log('Listing all notes!')
+});
+
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read note',
+  handler: () => console.log('Reading a note!')
+});
+
+console.log(yargs.argv);
