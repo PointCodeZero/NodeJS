ddd60bdc1f7ce70f3a751ae4e619899c9e6149f4
Lucas Torres <lucas.torres@myblueprint.ca>
2021-01-31 10:36:02 -0500

    test process.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,4 +2,6 @@ const chalk = require('chalk');
 const getNotes = require('./notes');
 
 console.log(getNotes());
-console.log(chalk.green.bold('Success!'));
+console.log(chalk.green.inverse.bold.italic('Success!'));
+
+console.log(process.argv[2])
