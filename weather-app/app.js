ca5c7baf79b8d2595bd47afe56be4edf0d3a9794
Lucas Torres <lucas.torres@myblueprint.ca>
2021-03-27 10:44:56 -0400

    fix key

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 require('dotenv').config()
 
-const url = `${process.env.WEATHERSTACK_BASE_URL}/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=37.8267,-122.4233&unit=f`;
+const url = `${process.env.WEATHERSTACK_BASE_URL}/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=37.8267,-122.4233&units=f`;
 
 request({ url, json: true }, (_err, _res, body) => {
   const { feelslike, temperature } = body.current;
