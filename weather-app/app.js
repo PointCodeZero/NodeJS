b2ebc03c091374f2d83cd47b20f135a012b54a22
Lucas Torres <lucas.torres@myblueprint.ca>
2021-03-27 10:43:48 -0400

    test weather stack api request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,9 @@
-console.log('Starting');
+const request = require('postman-request');
+require('dotenv').config()
 
-setTimeout(() => {
-  console.log('2s timer');
-}, 2000);
+const url = `${process.env.WEATHERSTACK_BASE_URL}/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=37.8267,-122.4233&unit=f`;
 
-console.log('Stopping');
+request({ url, json: true }, (_err, _res, body) => {
+  const { feelslike, temperature } = body.current;
+  console.log(`It is currently ${temperature} degrees out. But feels like ${feelslike} degrees!`);
+});
